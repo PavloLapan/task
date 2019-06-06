7ea632f804ce1af1dac0fe7766352609e8e0b9e5
Pavlo Lapan <pavlo042@gmail.com>
2019-06-06 22:49:34 +0300

    General changes + about
    changes with a lot of website, added about, need to finish media and add images to bookpage

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 $(".fa-chevron-down").click(function() {
     $('html,body').animate({
-        scrollTop: $(".booking-container").offset().top},
+        scrollTop: $(".about-us").offset().top},
         'slow');
 });
 
